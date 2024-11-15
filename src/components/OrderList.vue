352e1b436334eba755a6fc7fed77cf14c7140802
WillianDDaniel <williandeivitidaniel@live.com>
2024-11-15 10:18:27 -0300

    style: added style to order list component

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -4,6 +4,7 @@
 
   export default {
     name: 'OrderList',
+
     props: {
       orders: Array,
       showOrderDetails: Function,
@@ -13,31 +14,33 @@
       translateStatus(status) {
         return translateOrderStatus(status)
       },
-
       dateFormatter(date) {
         return dateFormatter(date)
       },
+    },
+
+    computed: {
+      sortedOrders() {
+        return this.orders.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+      }
     }
   }
 </script>
 
 <template>
   <section>
-
     <div class="order-list">
 
       <div
-        v-for="order in orders.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))"
-        @click="showOrderDetails(order.code)"
+        v-for="order in sortedOrders" @click="showOrderDetails(order.code)"
         class="order" v-bind:title="'Ver detalhes do pedido ' + order.code"
       >
-        <div>
-          Pedido: {{ order.code }} - {{ order.customer_name }}
+        <div class="order-header">
+          <span class="code">#{{ order.code }}</span>
+          {{ order.customer_name.split(' ')[0] }}
         </div>
 
-        <div>
-          Status: {{ translateStatus(order.status) }}
-        </div>
+        <div class="status">{{ translateStatus(order.status) }}</div>
 
         <div>
           Feito às {{ dateFormatter(order.created_at) }}
@@ -45,7 +48,6 @@
       </div>
 
     </div>
-
   </section>
 </template>
 
@@ -53,10 +55,17 @@
   .order-list {
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    gap: 7px;
+    padding: 5px 10px;
+    min-width: 330px;
+    max-height: calc(100vh - 100px);
+    overflow-y: scroll;
   }
 
   .order {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
     border: 1px solid #7b7b7b;
     padding: 10px;
     border-radius: 5px;
@@ -64,7 +73,27 @@
   }
 
   .order:hover {
-    background-color: #7b7b7b;
-    color: #fff;
+    border: 1px solid #27272a;
+    background-color: rgb(240, 240, 240);
+  }
+
+  .order-header {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .order-header .code {
+    font-weight: bold;
+    font-size: 0.9rem;
+    color: rgb(26, 26, 26);
+    border: 1px solid #7b7b7b;
+    border-radius: 3px;
+    padding: 1px 2px;
+  }
+
+  .status {
+    margin: 3px;
+    font-weight: 500;
   }
 </style>
